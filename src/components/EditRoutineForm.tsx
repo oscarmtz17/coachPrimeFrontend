fae3822352f0eb92cc60f4d104ab04377c1adbd7
Oscar Martinez <oscar1793@hotmail.com>
2024-11-13 01:26:49 -0600

    EditRoutineForm structure refactor

--- src/components/EditRoutineForm.tsx
+++ src/components/EditRoutineForm.tsx
@@ -1,25 +1,9 @@
-import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import api from "../services/api";
+// src/components/EditRoutineForm.tsx
+import React from "react";
+import EditRoutineFormStyles from "../styles/EditRoutineFormStyles";
+import { useEditRoutineForm } from "../hooks/useEditRoutineForm";
 import Modal from "./Modal";
 import ImageSelector from "./ImageSelector";
-
-interface DiaEntrenamiento {
-  diaSemana: string;
-  agrupaciones: Agrupacion[];
-}
-
-interface Agrupacion {
-  tipo: string;
-  ejercicios: Ejercicio[];
-}
-
-interface Ejercicio {
-  nombre: string;
-  series: number;
-  repeticiones: number;
-  imagenUrl: string;
-}
 
 interface EditRoutineFormProps {
   rutinaId: number;
@@ -32,257 +16,76 @@ const EditRoutineForm: React.FC<EditRoutineFormProps> = ({
   onRoutineUpdated,
   onClose,
 }) => {
-  const [nombre, setNombre] = useState("");
-  const [descripcion, setDescripcion] = useState("");
-  const [diasEntrenamiento, setDiasEntrenamiento] = useState<
-    DiaEntrenamiento[]
-  >([]);
-  const [error, setError] = useState<string | null>(null);
-  const [successMessage, setSuccessMessage] = useState<string | null>(null);
-  const [isImageSelectorOpen, setIsImageSelectorOpen] = useState(false);
-  const [selectedExercise, setSelectedExercise] = useState<{
-    dayIndex: number;
-    groupIndex: number;
-    exerciseIndex: number;
-  } | null>(null);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const fetchRoutineDetails = async () => {
-      try {
-        const response = await api.get(`/rutina/${rutinaId}`);
-        const adaptedRoutine = {
-          ...response.data,
-          diasEntrenamiento: response.data.diasEntrenamiento.$values.map(
-            (dia: any) => ({
-              ...dia,
-              agrupaciones: dia.agrupaciones.$values.map((agrupacion: any) => ({
-                ...agrupacion,
-                ejercicios: agrupacion.ejerciciosAgrupados.$values.map(
-                  (ejercicioAgrupado: any) => ejercicioAgrupado.ejercicio
-                ),
-              })),
-            })
-          ),
-        };
-        setNombre(adaptedRoutine.nombre);
-        setDescripcion(adaptedRoutine.descripcion);
-        setDiasEntrenamiento(adaptedRoutine.diasEntrenamiento);
-      } catch (error) {
-        setError("Error al cargar la rutina.");
-        console.error(error);
-      }
-    };
-    fetchRoutineDetails();
-  }, [rutinaId]);
-
-  const handleDayChange = (index: number, diaSemana: string) => {
-    const updatedDays = [...diasEntrenamiento];
-    updatedDays[index].diaSemana = diaSemana;
-    setDiasEntrenamiento(updatedDays);
-  };
-
-  const handleAddDay = () => {
-    const newDay: DiaEntrenamiento = { diaSemana: "", agrupaciones: [] };
-    setDiasEntrenamiento([...diasEntrenamiento, newDay]);
-  };
-
-  const handleRemoveDay = (dayIndex: number) => {
-    const updatedDays = [...diasEntrenamiento];
-    updatedDays.splice(dayIndex, 1);
-    setDiasEntrenamiento(updatedDays);
-  };
-
-  const handleAddGroup = (dayIndex: number, tipo: string) => {
-    const newGroup: Agrupacion = {
-      tipo,
-      ejercicios: createInitialExercises(tipo),
-    };
-    const updatedDays = [...diasEntrenamiento];
-    updatedDays[dayIndex].agrupaciones.push(newGroup);
-    setDiasEntrenamiento(updatedDays);
-  };
-
-  const handleRemoveGroup = (dayIndex: number, groupIndex: number) => {
-    const updatedDays = [...diasEntrenamiento];
-    updatedDays[dayIndex].agrupaciones.splice(groupIndex, 1);
-    setDiasEntrenamiento(updatedDays);
-  };
-
-  const createInitialExercises = (tipo: string): Ejercicio[] => {
-    let exerciseCount = 1;
-    switch (tipo) {
-      case "Bi-Serie":
-        exerciseCount = 2;
-        break;
-      case "Tri-Serie":
-        exerciseCount = 3;
-        break;
-      case "Cuatri-Serie":
-        exerciseCount = 4;
-        break;
-      case "Circuito":
-        exerciseCount = 0;
-        break;
-      default:
-        exerciseCount = 1;
-    }
-
-    return Array.from({ length: exerciseCount }, () => ({
-      nombre: "",
-      series: 1,
-      repeticiones: 1,
-      imagenUrl: "",
-    }));
-  };
-
-  const handleAddExercise = (dayIndex: number, groupIndex: number) => {
-    const newExercise: Ejercicio = {
-      nombre: "",
-      series: 1,
-      repeticiones: 1,
-      imagenUrl: "",
-    };
-    const updatedDays = [...diasEntrenamiento];
-    updatedDays[dayIndex].agrupaciones[groupIndex].ejercicios.push(newExercise);
-    setDiasEntrenamiento(updatedDays);
-  };
-
-  const handleRemoveExercise = (
-    dayIndex: number,
-    groupIndex: number,
-    exerciseIndex: number
-  ) => {
-    const updatedDays = [...diasEntrenamiento];
-    updatedDays[dayIndex].agrupaciones[groupIndex].ejercicios.splice(
-      exerciseIndex,
-      1
-    );
-    setDiasEntrenamiento(updatedDays);
-  };
-
-  const handleExerciseChange = (
-    dayIndex: number,
-    groupIndex: number,
-    exerciseIndex: number,
-    field: keyof Ejercicio,
-    value: string | number
-  ) => {
-    const updatedDays = [...diasEntrenamiento];
-    const group = updatedDays[dayIndex].agrupaciones[groupIndex];
-    group.ejercicios[exerciseIndex] = {
-      ...group.ejercicios[exerciseIndex],
-      [field]: value,
-    };
-    setDiasEntrenamiento(updatedDays);
-  };
-
-  const handleUpdateRoutine = async () => {
-    try {
-      const routine = {
-        nombre,
-        descripcion,
-        clienteId: 14,
-        usuarioId: 17,
-        diasEntrenamiento: diasEntrenamiento.map((dia) => ({
-          diaSemana: dia.diaSemana,
-          agrupaciones: dia.agrupaciones.map((agrupacion) => ({
-            tipo: agrupacion.tipo,
-            ejercicios: agrupacion.ejercicios,
-          })),
-        })),
-      };
-
-      const response = await api.put(`/rutina/${rutinaId}`, routine);
-
-      setSuccessMessage(response.data);
-      setError(null);
-      alert(response.data);
-
-      onRoutineUpdated();
-      navigate("/dashboard");
-    } catch (err: any) {
-      setError(err.response?.data || "Error al actualizar la rutina");
-      setSuccessMessage(null);
-      console.error(err);
-    }
-  };
-
-  // Funciones específicas para seleccionar y eliminar imágenes
-  const openImageSelector = (
-    dayIndex: number,
-    groupIndex: number,
-    exerciseIndex: number
-  ) => {
-    setSelectedExercise({ dayIndex, groupIndex, exerciseIndex });
-    setIsImageSelectorOpen(true);
-  };
-
-  const closeImageSelector = () => setIsImageSelectorOpen(false);
-
-  const handleSelectImage = (url: string) => {
-    if (selectedExercise) {
-      const { dayIndex, groupIndex, exerciseIndex } = selectedExercise;
-      const updatedDays = [...diasEntrenamiento];
-      updatedDays[dayIndex].agrupaciones[groupIndex].ejercicios[
-        exerciseIndex
-      ].imagenUrl = url;
-      setDiasEntrenamiento(updatedDays);
-    }
-    closeImageSelector();
-  };
-
-  const handleRemoveImage = (
-    dayIndex: number,
-    groupIndex: number,
-    exerciseIndex: number
-  ) => {
-    const updatedDays = [...diasEntrenamiento];
-    updatedDays[dayIndex].agrupaciones[groupIndex].ejercicios[
-      exerciseIndex
-    ].imagenUrl = "";
-    setDiasEntrenamiento(updatedDays);
-  };
+  const {
+    nombre,
+    setNombre,
+    descripcion,
+    setDescripcion,
+    diasEntrenamiento,
+    error,
+    successMessage,
+    handleUpdateRoutine,
+    handleAddDay,
+    handleRemoveDay,
+    handleDayChange,
+    handleAddGroup,
+    handleRemoveGroup,
+    handleExerciseChange,
+    handleAddExercise,
+    handleRemoveExercise,
+    isImageSelectorOpen,
+    openImageSelector,
+    closeImageSelector,
+    handleSelectImage,
+    handleRemoveImage,
+  } = useEditRoutineForm(rutinaId, onRoutineUpdated, onClose);
 
   return (
-    <div style={containerStyle}>
-      <h3 style={titleStyle}>Editar Rutina</h3>
-      {error && <p style={errorStyle}>{error}</p>}
-      {successMessage && <p style={successStyle}>{successMessage}</p>}
-      <div style={inputContainerStyle}>
-        <label style={labelStyle}>Nombre de la Rutina:</label>
+    <div style={EditRoutineFormStyles.container}>
+      <h3 style={EditRoutineFormStyles.title}>Editar Rutina</h3>
+      {error && <p style={EditRoutineFormStyles.error}>{error}</p>}
+      {successMessage && (
+        <p style={EditRoutineFormStyles.success}>{successMessage}</p>
+      )}
+
+      <div style={EditRoutineFormStyles.inputContainer}>
+        <label style={EditRoutineFormStyles.label}>Nombre de la Rutina:</label>
         <input
           value={nombre}
           onChange={(e) => setNombre(e.target.value)}
-          style={inputStyle}
+          style={EditRoutineFormStyles.input}
         />
       </div>
-      <div style={inputContainerStyle}>
-        <label style={labelStyle}>Descripción:</label>
+
+      <div style={EditRoutineFormStyles.inputContainer}>
+        <label style={EditRoutineFormStyles.label}>Descripción:</label>
         <textarea
           value={descripcion}
           onChange={(e) => setDescripcion(e.target.value)}
-          style={textareaStyle}
+          style={EditRoutineFormStyles.textarea}
         />
       </div>
-      <button onClick={handleAddDay} style={addButtonStyle}>
+
+      <button onClick={handleAddDay} style={EditRoutineFormStyles.addButton}>
         Agregar Día de Entrenamiento
       </button>
+
       {diasEntrenamiento.map((dia, dayIndex) => (
-        <div key={dayIndex} style={dayContainerStyle}>
-          <div style={dayHeaderStyle}>
-            <h4 style={dayTitleStyle}>Día de la Semana</h4>
+        <div key={dayIndex} style={EditRoutineFormStyles.dayContainer}>
+          <div style={EditRoutineFormStyles.dayHeader}>
+            <h4 style={EditRoutineFormStyles.dayTitle}>Día de la Semana</h4>
             <button
               onClick={() => handleRemoveDay(dayIndex)}
-              style={removeButtonStyle}
+              style={EditRoutineFormStyles.removeButton}
             >
               Quitar Día
             </button>
           </div>
+
           <select
             value={dia.diaSemana}
             onChange={(e) => handleDayChange(dayIndex, e.target.value)}
-            style={selectStyle}
+            style={EditRoutineFormStyles.select}
           >
             <option value="">Selecciona un día</option>
             <option value="Lunes">Lunes</option>
@@ -293,9 +96,11 @@ const EditRoutineForm: React.FC<EditRoutineFormProps> = ({
             <option value="Sábado">Sábado</option>
             <option value="Domingo">Domingo</option>
           </select>
+
+          <h4 style={EditRoutineFormStyles.subtitle}>Agrupación</h4>
           <select
             onChange={(e) => handleAddGroup(dayIndex, e.target.value)}
-            style={selectStyle}
+            style={EditRoutineFormStyles.select}
           >
             <option value="">Selecciona una agrupación</option>
             <option value="Ejercicio Individual">Ejercicio Individual</option>
@@ -304,20 +109,29 @@ const EditRoutineForm: React.FC<EditRoutineFormProps> = ({
             <option value="Cuatri-Serie">Cuatri-Serie</option>
             <option value="Circuito">Circuito</option>
           </select>
+
           {dia.agrupaciones.map((agrupacion, groupIndex) => (
-            <div key={groupIndex} style={groupContainerStyle}>
-              <div style={groupHeaderStyle}>
-                <h5 style={groupTitleStyle}>{agrupacion.tipo}</h5>
+            <div key={groupIndex} style={EditRoutineFormStyles.groupContainer}>
+              <div style={EditRoutineFormStyles.groupHeader}>
+                <h5 style={EditRoutineFormStyles.groupTitle}>
+                  {agrupacion.tipo}
+                </h5>
                 <button
                   onClick={() => handleRemoveGroup(dayIndex, groupIndex)}
-                  style={removeButtonStyle}
+                  style={EditRoutineFormStyles.removeButton}
                 >
                   Quitar Agrupación
                 </button>
               </div>
+
               {agrupacion.ejercicios.map((ejercicio, exerciseIndex) => (
-                <div key={exerciseIndex} style={exerciseContainerStyle}>
-                  <label style={exerciseLabelStyle}>Nombre del Ejercicio</label>
+                <div
+                  key={exerciseIndex}
+                  style={EditRoutineFormStyles.exerciseContainer}
+                >
+                  <label style={EditRoutineFormStyles.exerciseLabel}>
+                    Nombre del Ejercicio
+                  </label>
                   <input
                     value={ejercicio.nombre}
                     onChange={(e) =>
@@ -329,9 +143,11 @@ const EditRoutineForm: React.FC<EditRoutineFormProps> = ({
                         e.target.value
                       )
                     }
-                    style={inputStyle}
+                    style={EditRoutineFormStyles.input}
                   />
-                  <label style={exerciseLabelStyle}>Series</label>
+                  <label style={EditRoutineFormStyles.exerciseLabel}>
+                    Series
+                  </label>
                   <input
                     type="number"
                     value={ejercicio.series}
@@ -345,9 +161,11 @@ const EditRoutineForm: React.FC<EditRoutineFormProps> = ({
                         Number(e.target.value)
                       )
                     }
-                    style={inputStyle}
+                    style={EditRoutineFormStyles.input}
                   />
-                  <label style={exerciseLabelStyle}>Repeticiones</label>
+                  <label style={EditRoutineFormStyles.exerciseLabel}>
+                    Repeticiones
+                  </label>
                   <input
                     type="number"
                     value={ejercicio.repeticiones}
@@ -361,8 +179,9 @@ const EditRoutineForm: React.FC<EditRoutineFormProps> = ({
                         Number(e.target.value)
                       )
                     }
-                    style={inputStyle}
+                    style={EditRoutineFormStyles.input}
                   />
+
                   {ejercicio.imagenUrl ? (
                     <div style={{ display: "flex", alignItems: "center" }}>
                       <img
@@ -378,7 +197,7 @@ const EditRoutineForm: React.FC<EditRoutineFormProps> = ({
                         onClick={() =>
                           handleRemoveImage(dayIndex, groupIndex, exerciseIndex)
                         }
-                        style={removeButtonStyle}
+                        style={EditRoutineFormStyles.removeButton}
                       >
                         X
                       </button>
@@ -388,16 +207,17 @@ const EditRoutineForm: React.FC<EditRoutineFormProps> = ({
                       onClick={() =>
                         openImageSelector(dayIndex, groupIndex, exerciseIndex)
                       }
-                      style={addButtonStyle}
+                      style={EditRoutineFormStyles.addButton}
                     >
                       Seleccionar Imagen para Ejercicio
                     </button>
                   )}
                 </div>
               ))}
+
               <button
                 onClick={() => handleAddExercise(dayIndex, groupIndex)}
-                style={addButtonStyle}
+                style={EditRoutineFormStyles.addButton}
               >
                 Agregar Ejercicio
               </button>
@@ -405,11 +225,15 @@ const EditRoutineForm: React.FC<EditRoutineFormProps> = ({
           ))}
         </div>
       ))}
-      <div style={buttonContainerStyle}>
-        <button onClick={handleUpdateRoutine} style={saveButtonStyle}>
+
+      <div style={EditRoutineFormStyles.buttonContainer}>
+        <button
+          onClick={handleUpdateRoutine}
+          style={EditRoutineFormStyles.saveButton}
+        >
           Guardar Cambios
         </button>
-        <button onClick={onClose} style={cancelButtonStyle}>
+        <button onClick={onClose} style={EditRoutineFormStyles.cancelButton}>
           Cerrar
         </button>
       </div>
@@ -419,168 +243,6 @@ const EditRoutineForm: React.FC<EditRoutineFormProps> = ({
       </Modal>
     </div>
   );
-};
-
-// Mantén todos los estilos originales aquí
-
-// Styles
-const containerStyle: React.CSSProperties = {
-  backgroundColor: "#333",
-  color: "#fff",
-  padding: "2rem",
-  borderRadius: "8px",
-  width: "100%", // Para que ocupe el ancho completo
-  maxWidth: "800px", // Ajuste adicional para controlar el ancho máximo
-  margin: "0 auto",
-};
-
-const titleStyle: React.CSSProperties = {
-  color: "#ffcc00",
-  fontSize: "1.8rem",
-  textAlign: "center",
-  marginBottom: "1rem",
-};
-
-const errorStyle: React.CSSProperties = {
-  color: "red",
-  textAlign: "center",
-  marginBottom: "1rem",
-};
-
-const successStyle: React.CSSProperties = {
-  color: "green",
-  textAlign: "center",
-  marginBottom: "1rem",
-};
-
-const inputContainerStyle: React.CSSProperties = {
-  display: "flex",
-  flexDirection: "column",
-  gap: "0.5rem",
-  marginBottom: "1rem",
-};
-
-const labelStyle: React.CSSProperties = {
-  color: "#ffcc00",
-  fontWeight: "bold",
-};
-
-const dayHeaderStyle: React.CSSProperties = {
-  display: "flex",
-  justifyContent: "space-between",
-  alignItems: "center",
-};
-
-const groupHeaderStyle: React.CSSProperties = {
-  display: "flex",
-  justifyContent: "space-between",
-  alignItems: "center",
-};
-
-const removeButtonStyle: React.CSSProperties = {
-  backgroundColor: "#dc3545",
-  color: "#fff",
-  border: "none",
-  padding: "0.3rem 0.6rem",
-  borderRadius: "4px",
-  cursor: "pointer",
-  fontSize: "0.9rem",
-};
-
-const exerciseLabelStyle: React.CSSProperties = {
-  color: "#ffcc00",
-  fontSize: "0.9rem",
-  fontWeight: "bold",
-  marginBottom: "0.3rem",
-};
-
-const dayTitleStyle: React.CSSProperties = {
-  color: "#ffcc00",
-  fontSize: "1.2rem",
-};
-
-const dayContainerStyle: React.CSSProperties = {
-  backgroundColor: "#444",
-  padding: "1rem",
-  borderRadius: "8px",
-  marginBottom: "1rem",
-};
-
-const groupContainerStyle: React.CSSProperties = {
-  padding: "0.5rem",
-  borderRadius: "4px",
-  backgroundColor: "#555",
-  marginTop: "0.5rem",
-};
-
-const groupTitleStyle: React.CSSProperties = {
-  fontWeight: "bold",
-  color: "#ffcc00",
-};
-
-const exerciseContainerStyle: React.CSSProperties = {
-  display: "flex",
-  flexDirection: "column",
-  gap: "0.5rem",
-  marginBottom: "0.5rem",
-};
-
-const inputStyle: React.CSSProperties = {
-  padding: "0.5rem",
-  borderRadius: "4px",
-  border: "1px solid #ccc",
-  backgroundColor: "#555",
-  color: "#fff",
-};
-
-const textareaStyle: React.CSSProperties = {
-  padding: "0.5rem",
-  borderRadius: "4px",
-  border: "1px solid #ccc",
-  backgroundColor: "#555",
-  color: "#fff",
-  resize: "vertical",
-};
-
-const selectStyle: React.CSSProperties = {
-  padding: "0.5rem",
-  borderRadius: "4px",
-  border: "1px solid #ccc",
-  backgroundColor: "#555",
-  color: "#fff",
-};
-
-const addButtonStyle: React.CSSProperties = {
-  backgroundColor: "#007bff",
-  color: "#fff",
-  padding: "0.5rem",
-  borderRadius: "4px",
-  cursor: "pointer",
-  marginTop: "1rem",
-};
-
-const buttonContainerStyle: React.CSSProperties = {
-  display: "flex",
-  justifyContent: "space-between",
-  marginTop: "1rem",
-};
-
-const saveButtonStyle: React.CSSProperties = {
-  backgroundColor: "#ffcc00",
-  color: "#000",
-  padding: "0.5rem 1rem",
-  border: "none",
-  borderRadius: "4px",
-  cursor: "pointer",
-};
-
-const cancelButtonStyle: React.CSSProperties = {
-  backgroundColor: "#bbb",
-  color: "#333",
-  padding: "0.5rem 1rem",
-  border: "none",
-  borderRadius: "4px",
-  cursor: "pointer",
 };
 
 export default EditRoutineForm;
---
+++ src/hooks/useEditRoutineForm.ts
@@ -0,0 +1,259 @@
+// src/hooks/useEditRoutineForm.ts
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import api from "../services/api";
+
+interface DiaEntrenamiento {
+  diaSemana: string;
+  agrupaciones: Agrupacion[];
+}
+
+interface Agrupacion {
+  tipo: string;
+  ejercicios: Ejercicio[];
+}
+
+interface Ejercicio {
+  nombre: string;
+  series: number;
+  repeticiones: number;
+  imagenUrl: string;
+}
+
+export const useEditRoutineForm = (
+  rutinaId: number,
+  onRoutineUpdated: () => void,
+  onClose: () => void
+) => {
+  const [nombre, setNombre] = useState("");
+  const [descripcion, setDescripcion] = useState("");
+  const [diasEntrenamiento, setDiasEntrenamiento] = useState<
+    DiaEntrenamiento[]
+  >([]);
+  const [error, setError] = useState<string | null>(null);
+  const [successMessage, setSuccessMessage] = useState<string | null>(null);
+  const [isImageSelectorOpen, setIsImageSelectorOpen] = useState(false);
+  const [selectedExercise, setSelectedExercise] = useState<{
+    dayIndex: number;
+    groupIndex: number;
+    exerciseIndex: number;
+  } | null>(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchRoutineDetails = async () => {
+      try {
+        const response = await api.get(`/rutina/${rutinaId}`);
+        const adaptedRoutine = {
+          ...response.data,
+          diasEntrenamiento: response.data.diasEntrenamiento.$values.map(
+            (dia: any) => ({
+              ...dia,
+              agrupaciones: dia.agrupaciones.$values.map((agrupacion: any) => ({
+                ...agrupacion,
+                ejercicios: agrupacion.ejerciciosAgrupados.$values.map(
+                  (ejercicioAgrupado: any) => ejercicioAgrupado.ejercicio
+                ),
+              })),
+            })
+          ),
+        };
+        setNombre(adaptedRoutine.nombre);
+        setDescripcion(adaptedRoutine.descripcion);
+        setDiasEntrenamiento(adaptedRoutine.diasEntrenamiento);
+      } catch (error) {
+        setError("Error al cargar la rutina.");
+      }
+    };
+    fetchRoutineDetails();
+  }, [rutinaId]);
+
+  const handleDayChange = (index: number, diaSemana: string) => {
+    const updatedDays = [...diasEntrenamiento];
+    updatedDays[index].diaSemana = diaSemana;
+    setDiasEntrenamiento(updatedDays);
+  };
+
+  const handleAddDay = () => {
+    const newDay: DiaEntrenamiento = { diaSemana: "", agrupaciones: [] };
+    setDiasEntrenamiento([...diasEntrenamiento, newDay]);
+  };
+
+  const handleRemoveDay = (dayIndex: number) => {
+    const updatedDays = [...diasEntrenamiento];
+    updatedDays.splice(dayIndex, 1);
+    setDiasEntrenamiento(updatedDays);
+  };
+
+  const handleAddGroup = (dayIndex: number, tipo: string) => {
+    const newGroup: Agrupacion = {
+      tipo,
+      ejercicios: createInitialExercises(tipo),
+    };
+    const updatedDays = [...diasEntrenamiento];
+    updatedDays[dayIndex].agrupaciones.push(newGroup);
+    setDiasEntrenamiento(updatedDays);
+  };
+
+  const handleRemoveGroup = (dayIndex: number, groupIndex: number) => {
+    const updatedDays = [...diasEntrenamiento];
+    updatedDays[dayIndex].agrupaciones.splice(groupIndex, 1);
+    setDiasEntrenamiento(updatedDays);
+  };
+
+  const createInitialExercises = (tipo: string): Ejercicio[] => {
+    let exerciseCount = 1;
+    switch (tipo) {
+      case "Bi-Serie":
+        exerciseCount = 2;
+        break;
+      case "Tri-Serie":
+        exerciseCount = 3;
+        break;
+      case "Cuatri-Serie":
+        exerciseCount = 4;
+        break;
+      case "Circuito":
+        exerciseCount = 0;
+        break;
+      default:
+        exerciseCount = 1;
+    }
+
+    return Array.from({ length: exerciseCount }, () => ({
+      nombre: "",
+      series: 1,
+      repeticiones: 1,
+      imagenUrl: "",
+    }));
+  };
+
+  const handleAddExercise = (dayIndex: number, groupIndex: number) => {
+    const newExercise: Ejercicio = {
+      nombre: "",
+      series: 1,
+      repeticiones: 1,
+      imagenUrl: "",
+    };
+    const updatedDays = [...diasEntrenamiento];
+    updatedDays[dayIndex].agrupaciones[groupIndex].ejercicios.push(newExercise);
+    setDiasEntrenamiento(updatedDays);
+  };
+
+  const handleRemoveExercise = (
+    dayIndex: number,
+    groupIndex: number,
+    exerciseIndex: number
+  ) => {
+    const updatedDays = [...diasEntrenamiento];
+    updatedDays[dayIndex].agrupaciones[groupIndex].ejercicios.splice(
+      exerciseIndex,
+      1
+    );
+    setDiasEntrenamiento(updatedDays);
+  };
+
+  const handleExerciseChange = (
+    dayIndex: number,
+    groupIndex: number,
+    exerciseIndex: number,
+    field: keyof Ejercicio,
+    value: string | number
+  ) => {
+    const updatedDays = [...diasEntrenamiento];
+    const group = updatedDays[dayIndex].agrupaciones[groupIndex];
+    group.ejercicios[exerciseIndex] = {
+      ...group.ejercicios[exerciseIndex],
+      [field]: value,
+    };
+    setDiasEntrenamiento(updatedDays);
+  };
+
+  const handleUpdateRoutine = async () => {
+    try {
+      const routine = {
+        nombre,
+        descripcion,
+        clienteId: 14, // Esto podría ajustarse según tus necesidades
+        usuarioId: 17, // Esto también podría ajustarse
+        diasEntrenamiento: diasEntrenamiento.map((dia) => ({
+          diaSemana: dia.diaSemana,
+          agrupaciones: dia.agrupaciones.map((agrupacion) => ({
+            tipo: agrupacion.tipo,
+            ejercicios: agrupacion.ejercicios,
+          })),
+        })),
+      };
+
+      const response = await api.put(`/rutina/${rutinaId}`, routine);
+
+      setSuccessMessage(response.data);
+      setError(null);
+      onRoutineUpdated();
+      navigate("/dashboard");
+    } catch (err: any) {
+      setError(err.response?.data || "Error al actualizar la rutina");
+      setSuccessMessage(null);
+    }
+  };
+
+  // Funciones para seleccionar y eliminar imágenes
+  const openImageSelector = (
+    dayIndex: number,
+    groupIndex: number,
+    exerciseIndex: number
+  ) => {
+    setSelectedExercise({ dayIndex, groupIndex, exerciseIndex });
+    setIsImageSelectorOpen(true);
+  };
+
+  const closeImageSelector = () => setIsImageSelectorOpen(false);
+
+  const handleSelectImage = (url: string) => {
+    if (selectedExercise) {
+      const { dayIndex, groupIndex, exerciseIndex } = selectedExercise;
+      const updatedDays = [...diasEntrenamiento];
+      updatedDays[dayIndex].agrupaciones[groupIndex].ejercicios[
+        exerciseIndex
+      ].imagenUrl = url;
+      setDiasEntrenamiento(updatedDays);
+    }
+    closeImageSelector();
+  };
+
+  const handleRemoveImage = (
+    dayIndex: number,
+    groupIndex: number,
+    exerciseIndex: number
+  ) => {
+    const updatedDays = [...diasEntrenamiento];
+    updatedDays[dayIndex].agrupaciones[groupIndex].ejercicios[
+      exerciseIndex
+    ].imagenUrl = "";
+    setDiasEntrenamiento(updatedDays);
+  };
+
+  return {
+    nombre,
+    setNombre,
+    descripcion,
+    setDescripcion,
+    diasEntrenamiento,
+    error,
+    successMessage,
+    handleDayChange,
+    handleAddDay,
+    handleRemoveDay,
+    handleAddGroup,
+    handleRemoveGroup,
+    handleRemoveExercise,
+    handleExerciseChange,
+    handleAddExercise,
+    handleUpdateRoutine,
+    isImageSelectorOpen,
+    openImageSelector,
+    closeImageSelector,
+    handleSelectImage,
+    handleRemoveImage,
+  };
+};
