ad2be901b3f83f99eaeaecb5e35f5b564822eb0a
Oscar Martinez <oscar1793@hotmail.com>
2024-11-27 20:53:16 -0600

    AddRoutineForm modal Width modified

--- src/styles/AddRoutineFormStyles.ts
+++ src/styles/AddRoutineFormStyles.ts
@@ -9,6 +9,9 @@ const AddRoutineFormStyles: Record<string, CSSProperties> = {
     padding: "1.5rem",
     borderRadius: "8px",
     boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
+    width: "1000px",
+    minWidth: "300px",
+    margin: "0 auto",
   },
   title: {
     color: "#ffcc00",
