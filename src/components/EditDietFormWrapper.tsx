66e5598e2c894b357b7601d6d3e095027d83a73a
Oscar Martinez <oscar1793@hotmail.com>
2025-06-30 14:32:56 -0600

    i18next added and implemented in EditFormWrapper.tsx

--- src/components/EditDietFormWrapper.tsx
+++ src/components/EditDietFormWrapper.tsx
@@ -1,16 +1,31 @@
 // src/components/EditDietFormWrapper.tsx
 import React from "react";
+import { useTranslation } from "react-i18next";
 import { useParams } from "react-router-dom";
 import EditDietForm from "./EditDietForm";
 
 const EditDietFormWrapper: React.FC = () => {
+  const { t } = useTranslation();
   const { clienteId, dietaId } = useParams<{
     clienteId: string;
     dietaId: string;
   }>();
 
   if (!clienteId || !dietaId) {
-    return <p>Error: falta información de cliente o dieta.</p>;
+    return (
+      <div className="bg-zinc-900 min-h-screen flex justify-center items-center p-4 md:p-8">
+        <div className="bg-zinc-800 text-white p-8 rounded-lg max-w-4xl mx-auto">
+          <div className="flex justify-between items-center mb-4">
+            <h3 className="text-yellow-400 text-2xl text-center font-semibold">
+              {t("editDietWrapper.title")}
+            </h3>
+          </div>
+          <p className="text-red-500 text-center">
+            {t("editDietWrapper.error")}
+          </p>
+        </div>
+      </div>
+    );
   }
 
   return (
